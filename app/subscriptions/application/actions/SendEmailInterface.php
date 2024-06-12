6699b1111cecc450c011457abcf4491c512adbe7
dima.novoseltsev <dima.nowoseltsew@gmail.com>
2024-06-12 19:03:41 +0300

    chore: phpcs

--- app/subscriptions/application/actions/SendEmailInterface.php
+++ app/subscriptions/application/actions/SendEmailInterface.php
@@ -3,13 +3,14 @@
 namespace app\subscriptions\application\actions;
 
 use app\currencies\domain\entities\Currency;
+use app\subscriptions\application\dto\SendEmailDto;
 
 interface SendEmailInterface
 {
     /**
      * @param Currency $currency
-     * @param string $email
+     * @param SendEmailDto $dto
      * @return bool
      */
-    public function execute(Currency $currency, string $email): bool;
+    public function execute(Currency $currency, SendEmailDto $dto): bool;
 }
