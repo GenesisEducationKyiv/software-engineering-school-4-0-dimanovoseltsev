627aa45931598927592145da519584626c7df69a
dima.novoseltsev <dima.nowoseltsew@gmail.com>
2024-06-13 16:39:35 +0300

    chore: unit tests

---
+++ tests/unit/app/currencies/infrastructure/repositories/CurrencyRepositoryTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace tests\unit\app\currencies\infrastructure\repositories;
+
+use app\currencies\domain\entities\Currency;
+use app\currencies\infrastructure\models\CurrencyQuery;
+use app\currencies\infrastructure\repositories\CurrencyRepository;
+use app\shared\application\exceptions\NotValidException;
+use PHPUnit\Framework\MockObject\MockObject;
+use tests\unit\UnitTestCase;
+use yii\db\Exception;
+
+class CurrencyRepositoryTest extends UnitTestCase
+{
+    private CurrencyRepository $repository;
+    private CurrencyQuery|MockObject $query;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->query = $this->getCurrencyQueryMock();
+        $this->repository = new CurrencyRepository($this->query);
+    }
+
+    /**
+     * @return CurrencyQuery|MockObject
+     */
+    protected function getCurrencyQueryMock(): CurrencyQuery|MockObject
+    {
+        return $this->getMockBuilder(CurrencyQuery::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(
+                [
+                    'findByCode',
+                    'save',
+                ]
+            )
+            ->getMock();
+    }
+
+    public function testFindByCode()
+    {
+        $code = 'USD';
+        $model = $this->getCurrencyModelMock();
+        $model->id = 1;
+        $model->iso3 = $code;
+        $model->rate = 1;
+
+        $this->query->expects(self::once())
+            ->method('findByCode')
+            ->with($code)
+            ->willReturn($model);
+
+        $actual = $this->repository->findByCode($code);
+        self::assertInstanceOf(Currency::class, $actual);
+    }
+
+    public function testFindByCodeNotExit()
+    {
+        $code = 'USD';
+        $this->query->expects(self::once())
+            ->method('findByCode')
+            ->with($code)
+            ->willReturn(null);
+
+        $actual = $this->repository->findByCode($code);
+        self::assertEquals(null, $actual);
+    }
+
+    /**
+     * @throws NotValidException
+     * @throws Exception
+     */
+    public function testSave()
+    {
+        $entity = $this->getCurrencyEntity();
+
+        $code = 'USD';
+        $model = $this->getCurrencyModelMock();
+        $model->id = 1;
+        $model->iso3 = $code;
+        $model->rate = 1;
+
+        $this->query->expects(self::once())
+            ->method('save')
+            ->with([
+                'id' => $entity->getId()->value(),
+                'iso3' => $entity->getIso3()->value(),
+                'rate' => $entity->getRate()->value(),
+                'created_at' => $entity->getCreatedAt()->value(),
+                'updated_at' => $entity->getUpdatedAt()->value(),
+            ])
+            ->willReturn($model);
+
+        $actual = $this->repository->save($entity);
+        self::assertInstanceOf(Currency::class, $actual);
+    }
+}
+
