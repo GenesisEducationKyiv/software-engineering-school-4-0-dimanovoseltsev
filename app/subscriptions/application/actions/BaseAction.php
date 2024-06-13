66336bb4a50d4154b0765ebd9dfae9313ae480ff
dima.novoseltsev <dima.nowoseltsew@gmail.com>
2024-06-13 18:05:26 +0300

    chore: unit tests

--- app/subscriptions/application/actions/BaseAction.php
+++ app/subscriptions/application/actions/BaseAction.php
@@ -2,19 +2,6 @@
 
 namespace app\subscriptions\application\actions;
 
-use app\shared\application\exceptions\NotExistException;
-use app\subscriptions\domain\entities\Subscription;
-
 abstract class BaseAction
 {
-    /**
-     * @throws NotExistException
-     */
-    protected function checkExit(?Subscription $entity): Subscription
-    {
-        if ($entity === null) {
-            throw new NotExistException("Subscription not found");
-        }
-        return $entity;
-    }
 }
