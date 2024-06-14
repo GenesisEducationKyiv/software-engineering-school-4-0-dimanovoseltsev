20c8476ca20ed8030b2c8dd5313e9c1ec9cb921d
dima.novoseltsev <dima.nowoseltsew@gmail.com>
2024-06-14 09:37:41 +0300

    chore: refactoring

--- app/shared/application/exceptions/UnexpectedValueException.php
+++ app/shared/application/exceptions/UnexpectedValueException.php
@@ -10,4 +10,14 @@ use Throwable;
  */
 class UnexpectedValueException extends Exception implements Throwable
 {
+    /**
+     * UnexpectedValueException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $message = "Unexpected value", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
