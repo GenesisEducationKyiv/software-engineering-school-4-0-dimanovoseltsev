0e5d601805de0c1749b77d679d3d15bd1cba4b77
dima.novoseltsev <dima.nowoseltsew@gmail.com>
2024-06-29 16:03:34 +0300

    feat: migrate to microservice. change structure folders

--- docker/mysql/creaue_user.sh
+++ docker/mysql/creaue_user.sh
@@ -1,10 +1,14 @@
+ echo "CREATE Users"
+ mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 
 CREATE USER 'db_user'@'localhost' IDENTIFIED BY 'db_pass';
 GRANT ALL PRIVILEGES ON *.* TO 'db_user'@'localhost' WITH GRANT OPTION;
 
-CREATE USER 'db_user'@'%' IDENTIFIED BY 'db_pass';
-GRANT ALL PRIVILEGES ON *.* TO 'db_user'@'%' WITH GRANT OPTION;
+# CREATE USER 'db_user'@'%' IDENTIFIED BY 'db_pass';
+# GRANT ALL PRIVILEGES ON *.* TO 'db_user'@'%' WITH GRANT OPTION;
 
 SHOW GRANTS FOR db_user;
 FLUSH PRIVILEGES;
+
+ "
--- start.sh
+++ start.sh
@@ -3,5 +3,3 @@
 docker-compose pull
 docker-compose down -v --remove-orphans
 docker-compose up -d --build
-
-docker-compose logs -f app-setup
