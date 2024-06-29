7bda04594cce8c05caa9372e3d081fb9baba0d7c
dima.novoseltsev <dima.nowoseltsew@gmail.com>
2024-06-29 14:13:51 +0300

    feat: migrate to microservice. change structure folders

--- app/currencies/application/enums/CurrencyIso.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace app\currencies\application\enums;
-
-enum CurrencyIso: string
-{
-    case USD = 'USD';
-    case UAH = 'UAH';
-}
--- common/config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    "workers" => [
-        "mail" => [
-            "maxAttempt" => (int)getenv("SEND_EMAIL_QUEUE_MAX_ATTEMPT")
-        ]
-    ],
-];
