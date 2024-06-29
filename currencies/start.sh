8fe5d55dded1d089145364247843432cf40f2d6c
dima.novoseltsev <dima.nowoseltsew@gmail.com>
2024-06-29 19:53:28 +0300

    feat: migrate to microservice. change structure folders

--- currencies/start.sh
+++ currencies/start.sh
@@ -4,4 +4,4 @@ docker-compose pull
 docker-compose down -v --remove-orphans
 docker-compose up -d --build
 
-docker-compose logs -f app-setup
+docker-compose logs -f app-setup-currencies
--- currencies/tests/load-k6/retrieve-currency.js
+++ currencies/tests/load-k6/retrieve-currency.js
@@ -1,7 +1,7 @@
 import http from "k6/http";
 import {check, sleep} from "k6";
 
-const BASE_URL = __ENV.BASE_URL || 'http://nginx';
+const BASE_URL = __ENV.BASE_URL || 'http://nginx-currencies';
 
 export const options = {
     vus: 10,
